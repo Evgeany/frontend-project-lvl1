cfdda4bcf0061a29f2fc29aa0c8f4e1bbc1b48de
Evgeany Midonov <midonoff@gmail.com>
2022-02-10 17:16:22 +0400

    brain-games changed export to default

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const greetingsFunc = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name?: ');
-    console.log(`Hello, ${name}!`);
-}
+const greetingsFunc = () => {
+	console.log('Welcome to the Brain Games!');
+	const name = readlineSync.question('May I have your name?: ');
+	console.log(`Hello, ${name}!`);
+};
+
+export default greetingsFunc;
