e3ae42582f93f37fb5342a6f9a2cbcf8c5936a7a
Evgeany Midonov <midonoff@gmail.com>
2022-02-17 21:07:08 +0400

    brain-games add greetings to  brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+import { greetingPhrase, getName } from '../src/cli.js';
+console.log(greetingPhrase);
+const myName = getName();
+console.log(`Hello, ${myName}`);
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,4 +4,3 @@ import { greetingPhrase, getName } from '../src/cli.js';
 console.log(greetingPhrase);
 const myName = getName();
 console.log(`Hello, ${myName}`);
-console.log(`my name is ${myName}`)
