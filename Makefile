53badb04d499954c52e722b77657ca585baa801c
Evgeany Midonov <midonoff@gmail.com>
2022-02-17 21:09:56 +0400

    brain-games add brain-even to Makefile

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ install: # выполнение npm ci
 brain-games: # запуск brain-games
 	node bin/brain-games.js
 
+brain-even: # запуск brain-even
+	node bin/brain-even.js
+
 lint: # запуск npx eslint .
 	npx eslint .
 
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const greetingsFunc = () => {
-    console.log('Welcome to the Brain Games!');
+export const greetingPhrase = 'Welcome to the Brain Games!';
+
+export const getName = () => {
+
     const name = readlineSync.question('May I have your name?: ');
     return name;
 };
-
-export default greetingsFunc;
