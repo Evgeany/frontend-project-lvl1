5755c5e89074bc223be040acc6247d66b1e2aea4
Evgeany Midonov <midonoff@gmail.com>
2022-02-17 21:20:33 +0400

    brain-games add brain-even executable

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync';
 export const greetingPhrase = 'Welcome to the Brain Games!';
 
 export const getName = () => {
-
     const name = readlineSync.question('May I have your name?: ');
     return name;
 };
