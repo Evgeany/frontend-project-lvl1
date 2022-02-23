3e071c68c3782c01770cb92dfd5129f603253e42
Evgeany Midonov <midonoff@gmail.com>
2022-02-23 17:29:07 +0400

    brain-games add change to bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import runCalc from '../games/even-game.js';
+import runCalc from '../games/calc-game.js';
 
 // здесь будет запускаться игра путем экспортированния из логики файла - функцции.
 
