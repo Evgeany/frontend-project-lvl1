dc7026d64b124496c435ac9cc8699711b66fd583
Evgeany Midonov <midonoff@gmail.com>
2022-02-23 16:45:34 +0400

    brain-games add logic to games/even-game.js

---
+++ games/even-game.js
@@ -0,0 +1,20 @@
+import { getRandomInt } from '../src/utility.js';
+import { modelFunc } from '../src/index.js';
+
+//  здесь будет описываться логика для first-even.js
+
+// ******************** логика игры на четность *************************
+
+const question = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+
+
+const createTaskAndResult = () => {
+
+    const mathtask = getRandomInt(1, 100);
+    const result = mathtask % 2 === 0 ? 'yes' : 'no';
+
+    return [mathtask, result];
+};
+
+export default () => modelFunc(question, createTaskAndResult);
