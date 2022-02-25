3e0800a22a19e69d56cdf1483a91a198e7cbf865
Evgeany Midonov <midonoff@gmail.com>
2022-02-25 22:11:48 +0400

    brain-games fix  brain-progression.js

---
+++ bin/brain-progression.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import runProgression from '../games/progression-game.js';
+
+// здесь будет запускаться игра путем экспортированния из логики файла - функцции.
+
+runProgression();
