00a6487f096ed710384d8bcb26887b7be78410a3
Evgeany Midonov <midonoff@gmail.com>
2022-02-25 22:09:25 +0400

    brain-games add logic to progression-games.js

--- games/progression-game.js
+++ games/progression-game.js
@@ -1,4 +1,37 @@
 import getRandomInt from '../src/utility.js';
 import modelFunc from '../src/index.js';
 
+const question = 'What number is missing in the progression?';
+
+const getProgression = (startValue, growValue) => {
+    const progression = [];
+
+    for (let i = startValue; i <= startValue + 15; i += growValue) {
+        progression.push(i);
+    }
+
+    progression[getRandomInt(1, progression.length - 2)] = '..';
+
+    return progression;
+};
+
+const getResult = (arr, growValue) => {
+    let result = 0;
+
+    for (let i = 0; i <= arr.length - 1; i += 1) {
+        if (arr[i] === '..') { result = arr[i + 1] - growValue; }
+    }
+
+    return result;
+};
+
+const createTaskAndResult = () => {
+    const startValue = getRandomInt();
+    const growValue = getRandomInt(1, 3);
+    const mathtask = getProgression(startValue, growValue);
+    const result = getResult(mathtask, growValue);
+
+    return [mathtask, result.toString()];
+};
+
 export default () => modelFunc(question, createTaskAndResult);
