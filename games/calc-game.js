742beb924ebdae9686dd7ca313e2f948ef637471
Evgeany Midonov <midonoff@gmail.com>
2022-02-27 20:22:01 +0400

    brain-games add fix

--- games/calc-game.js
+++ games/calc-game.js
@@ -9,27 +9,27 @@ const question = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
 
 const getResult = (int1, int2, operand) => {
-    let mathresult = 0;
-    if (operand === '-') {
-        mathresult = int1 - int2;
-    }
-    if (operand === '+') {
-        mathresult = int1 + int2;
-    }
-    if (operand === '*') {
-        mathresult = int1 * int2;
-    }
-    return mathresult;
+  let mathresult = 0;
+  if (operand === '-') {
+    mathresult = int1 - int2;
+  }
+  if (operand === '+') {
+    mathresult = int1 + int2;
+  }
+  if (operand === '*') {
+    mathresult = int1 * int2;
+  }
+  return mathresult;
 };
 
 const createTaskAndResult = () => {
-    const num1 = getRandomInt();
-    const num2 = getRandomInt();
-    const operator = operators[getRandomInt(0, operators.length - 1)];
-    const mathtask = `${num1} ${operator} ${num2}`;
-    const result = `${getResult(num1, num2, operator)}`;
+  const num1 = getRandomInt();
+  const num2 = getRandomInt();
+  const operator = operators[getRandomInt(0, operators.length - 1)];
+  const mathtask = `${num1} ${operator} ${num2}`;
+  const result = `${getResult(num1, num2, operator)}`;
 
-    return [mathtask, result.toString()];
+  return [mathtask, result.toString()];
 };
 
 export default () => modelFunc(question, createTaskAndResult);
